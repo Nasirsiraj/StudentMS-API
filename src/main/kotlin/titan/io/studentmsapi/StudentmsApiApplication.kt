a170302d860a8081ba4c940a8293a0aefb645633
Nasirsiraj <mdnasir74725@gmail.com>
2021-01-07 10:24:28 +0600

    Initial commit

---
+++ src/main/kotlin/titan/io/studentmsapi/StudentmsApiApplication.kt
@@ -0,0 +1,11 @@
+package titan.io.studentmsapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudentmsApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<StudentmsApiApplication>(*args)
+}
